50b9061c6ce1427bdf6050bc9c6ffc91b4992afe
afmssrahi <afmssrahi@gmail.com>
2022-01-05 10:22:05 +0600

    Read Me File edit for basic Information

--- src/components/ListOfCategory.js
+++ src/components/ListOfCategory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ListOfCategory = () => {
 	return (
 		<div className='view'>
-			<h1>This is List of Category.</h1>
+			<h4>This is List of Category.</h4>
 		</div>
 	);
 };
